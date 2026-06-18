ad5d4bf291b6f696e66a5b87d2b3a8cdf02b90be
Ronny Bergmann <git@ronnybergmann.net>
2026-06-18 10:56:35 +0200

    back to documenter – deactivate versions.

--- docs/make.jl
+++ docs/make.jl
@@ -8,87 +8,45 @@ if "--help" ∈ ARGS
         """
 docs/make.jl
 
-Render the `JuliaManifolds` GitHub Organisation Multidocumenter with optional arguments
+Render the `JuliaManifolds` GitHub Organisation documentation – a short overview on
+the Julia packages this organisation consists of.
 
 Arguments
-* `--deploy`       - deploy docs to GitHub pages (e.g. on CI)
 * `--help`         - print this help and exit without rendering the documentation
 * `--serve`        - use `LiveServer.jl` to serve the current docs, also launches the browser
-* `--temp`         – clone the other repositories into a temp folder – otherwise use clones/
 """,
     )
     exit(0)
 end
 
-
-# ## if docs is not the current active environment, switch to it
+run_on_CI = (get(ENV, "CI", nothing) == "true")
+#
+# (b) if docs is not the current active environment, switch to it
 # (from https://github.com/JuliaIO/HDF5.jl/pull/1020/) 
 if Base.active_project() != joinpath(@__DIR__, "Project.toml")
     using Pkg
     Pkg.activate(@__DIR__)
-    Pkg.resolve()
     Pkg.instantiate()
 end
 
-buildpath =  ("--temp" in ARGS) ? mktempdir() : joinpath(@__DIR__, "build")
 
-@info """
-Building docs into: $(buildpath)
-"""
-
-using LiveServer, Documenter
+using Documenter, LiveServer
 
 # Build local docs - but do bot deploy
 makedocs(;
     format=Documenter.HTML(;
-        prettyurls=false, assets=["assets/favicon.ico"]
+        prettyurls = run_on_CI, assets=["assets/favicon.ico"]
     ),
     modules=Module[],
     authors="Ronny Bergmann",
     sitename="Julia Manifolds",
     pages=[
         "Home" => "index.md",
-    ]
+    ],
+    version = ""
 )
 
-# ## Deploy
-if "--deploy" in ARGS
-    @warn "Deploying to GitHub"
-    gitroot = normpath(joinpath(@__DIR__, ".."))
-    run(`git pull`)
-    outbranch = "gh-pages"
-    has_outbranch = true
-    if !success(`git checkout $outbranch`)
-        has_outbranch = false
-        if !success(`git switch --orphan $outbranch`)
-            @error "Cannot create new orphaned branch $outbranch."
-            exit(1)
-        end
-    end
-    # Delete all current files on our current branch besides the .git folder
-    for file in readdir(gitroot; join = true)
-        endswith(file, ".git") && continue
-        rm(file; force = true, recursive = true)
-    end
-    # copy the rendered docs
-    for file in readdir(buildpath)
-        cp(joinpath(buildpath, file), joinpath(gitroot, file))
-    end
-    # Add and commit new files
-    run(`git add .`)
-    if success(`git commit -m 'Aggregate documentation'`)
-        @info "Pushing updated documentation."
-        if has_outbranch
-            run(`git push`)
-        else
-            run(`git push -u origin $outbranch`)
-        end
-        run(`git checkout main`)
-    else
-        @info "No changes to aggregated documentation."
-    end
-else
-    @info "Skipping deployment, '--deploy' not passed. Generated files in docs/$(buildpath)."
-end
+deploydocs(repo = "github.com/JuliaManifolds/juliamanifolds.github.io.git", push_preview = true)
+
 # ## Serve ?
-("--serve" in ARGS) && serve(dir=joinpath(@__DIR__, buildpath), launch_browser=true)
+("--serve" in ARGS) && serve(dir=joinpath(@__DIR__, "build"), launch_browser=true)
